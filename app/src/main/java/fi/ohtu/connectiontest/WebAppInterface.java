50c202c61946f5d974bd9f835e5157ea63d184e6
iilumme <iilumme@cs.helsinki.fi>
2016-08-04 12:47:02 +0300

    sends destinations in json form

--- app/src/main/java/fi/ohtu/connectiontest/WebAppInterface.java
+++ app/src/main/java/fi/ohtu/connectiontest/WebAppInterface.java
@@ -2,6 +2,9 @@ package fi.ohtu.connectiontest;
 
 import android.webkit.JavascriptInterface;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.List;
 
 import fi.ohtu.mobilityprofileapi.MessageCreator;
@@ -36,40 +39,42 @@ public class WebAppInterface {
     }
 
     private String convertListOfDestinations() {
+        JSONObject object = new JSONObject();
+        JSONArray destinationsArray = new JSONArray();
         List<String> destinations = messageHandler.getListOfMostProbableDestinations();
-        String dests = "";
+
         try {
-
             for (int i = 0; i < destinations.size() ; i++) {
-                if (i == destinations.size()-1) {
-                    dests += destinations.get(i);
-                } else {
-                    dests += (destinations.get(i) + "!");
-                }
-
+                destinationsArray.put(destinations.get(i));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return dests;
+        try {
+            object.put("destinations", destinationsArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return object.toString();
     }
 
     private String convertListOfTransportModes() {
+        JSONObject object = new JSONObject();
+        JSONArray modesArray = new JSONArray();
         List<String> modes = messageHandler.getListOfPreferredTransportModes();
-        String mods = "";
         try {
-
             for (int i = 0; i < modes.size() ; i++) {
-                if (i == modes.size()-1) {
-                    mods += modes.get(i);
-                } else {
-                    mods += (modes.get(i) + "!");
-                }
-
+                modesArray.put(modes.get(i));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return mods;
+        try {
+            object.put("transport_modes", modesArray);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return object.toString();
     }
 }
